2bef240a797ff6ea476086422aaca38e3eda048a
Andrew Buter <andrey.buter@gmail.com>
2021-05-12 12:14:20 +0200

    update route

--- back/src/routes/get-items.route.ts
+++ back/src/routes/get-items.route.ts
@@ -4,7 +4,7 @@ import { ExternalRequest } from "../utils/external-request"
 
 const router = Router();
 
-router.get( '/api/get-items/:path', async ( request, responce, next ) => {
+router.get( '/get/:path', async ( request, responce, next ) => {
     const path = decodeURIComponent(request.params.path)
         // remove all html tags
         // https://stackoverflow.com/questions/5002111/how-to-strip-html-tags-from-string-in-javascript
@@ -25,5 +25,10 @@ router.get( '/api/get-items/:path', async ( request, responce, next ) => {
     responce.header( 'Access-Control-Allow-Origin', '*' );
     responce.json( data );
 } );
+
+router.get('/*', (req, res) => {
+    // res.sendFile( path.join( __dirname,  '../frontend/dist/schedule/index.html'));
+    res.json('page loaded');
+});
 
 export default router;
